747a4798ab003d85874e027a0b0312e91cc4d1d5
Dian4ik <jakie16@yandex.ru>
2023-08-28 19:51:43 +0800

    12ПР добавлены ссылки в хэдере

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
 import headerLogo from '../images/logo.svg';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function Header() {
     return (
         <header className="header">
-            <img className="header__logo" src={headerLogo} alt="Логотип" />
+            <Link to="/">
+                <img className="header__logo" src={headerLogo} alt="Логотип" />
+            </Link>
+
+            <Routes>
+                <Route path="/" element={
+                    <div className="header__profile">
+                        <p className="header__email">email@example.com</p>
+                        <Link to="/sign-in" className="header__link">Выйти</Link>
+                    </div>
+                } />
+                <Route path="/sign-in" element={<Link to="/sign-up" className="header__link">Регистрация</Link>} />
+                <Route path="/sign-up" element={<Link to="/sign-in" className="header__link">Войти</Link>} />
+
+            </Routes>
+
         </header>
     );
 }
